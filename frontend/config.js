e5ec0b81c60157cc37fd18a7a35ea5fa107c1a70
andrecampos565-ctrl <andrecampos565@gmail.com>
2026-07-20 23:09:32 +0200

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,13 @@
+// Configuração do Quiosque IEAFE BE
+
+const configuracao = {
+
+    nome: "IEAFE BE",
+
+    retornoTelaInicial: true,
+
+    tempoRetorno: 60000,
+
+    dispositivo: "iPad 12.9"
+
+};
